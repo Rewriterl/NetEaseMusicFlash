d6f3ef50ea939e1c3226c6605f335ef40c20c3d5
Rewriterl <1354413904@qq.com>
2020-05-19 17:10:50 +0800

    follow

--- src/server/follow.js
+++ src/server/follow.js
@@ -2,7 +2,7 @@
 
 module.exports = (query, request) => {
   query.cookie.os = 'pc'
-  query.t = (query.t === 1 ? 'follow' : 'delfollow')
+  query.t = 'follow'
   return request(
     'POST', `https://music.163.com/weapi/user/${query.t}/${query.id}`, {},
     {crypto: 'weapi', cookie: query.cookie, proxy: query.proxy}
